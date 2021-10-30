288713d06acf5b1f6150948c43dd9c213a0f074e
NA Shree Nitu <79525128+FreelancerNitu@users.noreply.github.com>
2021-10-31 02:46:03 +0600

    For this project I am using here HTML, CSS and JS

---
+++ Dammy text generate/js/app.js
@@ -0,0 +1,57 @@
+  const dumyText = [
+  '<h5>NA Shree Nitu</h5>',
+  '1.Ice cream cotton candy ice cream apple pie topping. Icing gingerbread biscuit lemon drops carrot cake. Shortbread toffee cookie candy cupcake soufflé cake. Donut chupa chups caramels brownie gummies apple pie soufflé. Chupa chups bear claw pie caramels lemon drops. Chupa chups fruitcake marshmallow marshmallow cotton candy. Topping chocolate cake tiramisu tiramisu jujubes. Cake sesame snaps tart pudding candy. Cupcake sesame snaps cookie muffin jelly-o pastry croissant wafer tiramisu.',
+
+  '2.Brownie danish muffin dessert chocolate cake. Topping wafer chocolate jelly beans croissant marshmallow halvah gingerbread. Oat cake brownie donut cake marzipan liquorice pie. Sesame snaps powder chocolate cake candy canes fruitcake muffin jelly. Croissant pastry sweet danish sesame snaps chocolate bar cake shortbread. Gingerbread dessert cheesecake powder dessert sweet roll liquorice cotton candy.',
+  
+  '3.Danish sesame snaps jujubes gingerbread cake lemon drops tootsie roll toffee. Cookie lemon drops lollipop tootsie roll cookie. Cheesecake candy canes lemon drops lollipop dragée dessert marshmallow. Chocolate oat cake jelly-o pie gummi bears. Dragée jelly topping topping sweet roll. Fruitcake lollipop ice cream tootsie roll powder.',
+  
+  '4.Dragée donut lollipop sugar plum dessert sweet roll lemon drops croissant. Gummies fruitcake fruitcake sesame snaps chocolate pastry danish jelly beans sesame snaps. Cheesecake tart jelly donut tootsie roll biscuit chocolate oat cake cheesecake. Tart caramels liquorice shortbread bonbon chocolate cake brownie. Jujubes pudding tart carrot cake apple pie halvah jujubes chocolate. Tart powder icing muffin lollipop.',
+  
+  '5.Cookie bear claw candy canes danish wafer. Ice cream dessert toffee powder cookie marshmallow chocolate cake. Liquorice croissant cheesecake lollipop pudding. Jujubes pie chupa chups powder tiramisu. Oat cake danish toffee bear claw dragée ice cream shortbread. Oat cake lemon drops cheesecake pie cheesecake chupa chups sweet apple pie. Sesame snaps bear claw muffin cake chocolate bar jujubes. Pie chupa chups toffee tootsie roll chocolate bar pudding bonbon. Jujubes marzipan biscuit gingerbread candy canes liquorice tootsie roll croissant. Danish jelly-o liquorice macaroon dessert dessert oat cake topping.',
+  
+  '6.Donut toffee jelly-o muffin sweet macaroon. Bonbon marzipan bonbon cookie biscuit. Croissant soufflé apple pie icing croissant bear claw chupa chups. Tootsie roll biscuit croissant gummi bears cake fruitcake candy canes. Sesame snaps chocolate chocolate cake lemon drops fruitcake tiramisu tiramisu gummi bears lemon drops. Cookie shortbread dessert gummi bears dragée lollipop toffee candy powder. Jelly candy sesame snaps fruitcake dessert cupcake pastry cake marzipan. Cake sweet biscuit icing candy cake tart. Soufflé tart cookie cake chupa chups carrot cake lollipop soufflé.',
+  
+  '7.Bear claw gummi bears sugar plum tootsie roll carrot cake brownie apple pie marshmallow candy. Lemon drops jelly beans soufflé bear claw caramels bonbon jelly beans pudding jelly-o. Chocolate cake sugar plum lollipop cake soufflé sesame snaps carrot cake. Lemon drops caramels croissant chocolate candy jelly-o chocolate bar. Pie candy sesame snaps toffee topping danish gingerbread cotton candy. Pie lemon drops lemon drops toffee marzipan biscuit dragée bonbon wafer.',
+  
+  '8.Biscuit jelly caramels candy canes danish gummies cake candy canes. Chupa chups dragée tiramisu sesame snaps jelly jelly dessert donut. Donut toffee shortbread lemon drops cake cake cotton candy. Chocolate cake caramels jelly-o apple pie jelly-o jelly-o icing wafer. Dessert cheesecake cake cheesecake pudding gingerbread apple pie shortbread. Jujubes muffin tiramisu cookie donut oat cake icing dessert icing.',
+  
+  '9.Gummi bears dragée icing pastry danish croissant wafer pie. Jelly marshmallow sugar plum muffin pastry jelly-o soufflé. Pie chupa chups jelly beans tart chocolate biscuit wafer dragée. Marshmallow croissant jujubes ice cream ice cream chupa chups. Donut cookie candy marshmallow powder bonbon halvah tart. Gummies muffin chocolate cake toffee danish sweet roll cake cookie. Cookie croissant biscuit cookie pie. Carrot cake bonbon bonbon lemon drops chocolate bar shortbread sweet roll dragée brownie.',
+  '10.Biscuit jelly caramels candy canes danish gummies cake candy canes. Chupa chups dragée tiramisu sesame snaps jelly jelly dessert donut. Donut toffee shortbread lemon drops cake cake cotton candy. Chocolate cake caramels jelly-o apple pie jelly-o jelly-o icing wafer. Dessert cheesecake cake cheesecake pudding gingerbread apple pie shortbread. Jujubes muffin tiramisu cookie donut oat cake icing dessert icing.',
+  
+  '11.Gummi bears dragée icing pastry danish croissant wafer pie. Jelly marshmallow sugar plum muffin pastry jelly-o soufflé. Pie chupa chups jelly beans tart chocolate biscuit wafer dragée. Marshmallow croissant jujubes ice cream ice cream chupa chups. Donut cookie candy marshmallow powder bonbon halvah tart. Gummies muffin chocolate cake toffee danish sweet roll cake cookie. Cookie croissant biscuit cookie pie. Carrot cake bonbon bonbon lemon drops chocolate bar shortbread sweet roll dragée brownie.',
+  '12.Biscuit jelly caramels candy canes danish gummies cake candy canes. Chupa chups dragée tiramisu sesame snaps jelly jelly dessert donut. Donut toffee shortbread lemon drops cake cake cotton candy. Chocolate cake caramels jelly-o apple pie jelly-o jelly-o icing wafer. Dessert cheesecake cake cheesecake pudding gingerbread apple pie shortbread. Jujubes muffin tiramisu cookie donut oat cake icing dessert icing.',
+  
+  '13.Gummi bears dragée icing pastry danish croissant wafer pie. Jelly marshmallow sugar plum muffin pastry jelly-o soufflé. Pie chupa chups jelly beans tart chocolate biscuit wafer dragée. Marshmallow croissant jujubes ice cream ice cream chupa chups. Donut cookie candy marshmallow powder bonbon halvah tart. Gummies muffin chocolate cake toffee danish sweet roll cake cookie. Cookie croissant biscuit cookie pie. Carrot cake bonbon bonbon lemon drops chocolate bar shortbread sweet roll dragée brownie.',
+  '14.Biscuit jelly caramels candy canes danish gummies cake candy canes. Chupa chups dragée tiramisu sesame snaps jelly jelly dessert donut. Donut toffee shortbread lemon drops cake cake cotton candy. Chocolate cake caramels jelly-o apple pie jelly-o jelly-o icing wafer. Dessert cheesecake cake cheesecake pudding gingerbread apple pie shortbread. Jujubes muffin tiramisu cookie donut oat cake icing dessert icing.',
+  
+  '15.Gummi bears dragée icing pastry danish croissant wafer pie. Jelly marshmallow sugar plum muffin pastry jelly-o soufflé. Pie chupa chups jelly beans tart chocolate biscuit wafer dragée. Marshmallow croissant jujubes ice cream ice cream chupa chups. Donut cookie candy marshmallow powder bonbon halvah tart. Gummies muffin chocolate cake toffee danish sweet roll cake cookie. Cookie croissant biscuit cookie pie. Carrot cake bonbon bonbon lemon drops chocolate bar shortbread sweet roll dragée brownie.',
+  ];
+  // Select elements
+  const form = document.querySelector('#form');
+  const amount = document.querySelector('#amount');
+  const result = document.querySelector('#result');
+
+  // Event listener
+  form.addEventListener('submit', function(e){
+  e.preventDefault();
+  
+  // User koto value dela ta bar kore
+  const value = parseInt(amount.value);
+  const random = Math.floor(Math.random() * dumyText.length);
+ 
+  // User chaila ja kunu number deta para Like:
+  //0
+  //nagative
+  // 9 ar thaka bora
+  //string
+  if(isNaN(value) || value <= 0 || value > 15){
+    result.innerHTML = `<p>${dumyText[random]} </p>`; //jode valu na thaka ba 0 kingba 9 ar boro hola 1 ta p deba
+  }else{
+    const sliceArray = dumyText.slice(0, value); //number na delaw 1 tar por 1 ta asba
+    const finalArray = sliceArray.map(function (singleDummy){
+      return `<p>${singleDummy}</p>`; // sa ak ta ak ta p tag returen korva
+    }).join();
+    result.innerHTML = finalArray; //atar madomma p gulu k display koralam
+  }
+})
